6fe3d389948dbc7f6ef8f2548bddb453385f5aa4
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:53:39 +0000

    Backup Commit

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,6 +1,14 @@
 import react from 'react';
 import './App.css'
 import { useState } from 'react';
-export const App=()=>{
-    const[name,setName]=uses
+export const App=(props)=>{
+    const[name,setName]=useState('');
+    return(
+        <>
+        <h2>Hey!!! Greeting</h2>
+        <form className='greeting' onSubmit={setter}>
+            <input 
+        </form>
+        </>
+    )
 }
